31a104cc78134c8850a86003d55ee4fbb5b71e73
Marcio <marcioabcd11@gmail.com>
2021-11-30 13:45:01 -0300

    ready to deploy

--- src/services/service.baseURL.js
+++ src/services/service.baseURL.js
@@ -1,9 +1,7 @@
 import process from 'process';
 
-const { NODE_ENV } = process.env;
-
-const BASE_URL = (NODE_ENV === 'production')
-  ? 'test'
+const BASE_URL = (process.env.REACT_APP_NODE_ENV === 'production')
+  ? ' https://zapper-marcio.herokuapp.com'
   : 'http://localhost:4000';
 
 export default BASE_URL;
