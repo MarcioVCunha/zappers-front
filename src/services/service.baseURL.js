0d7bbf887d1858b70b6f5fa615f80132ae46f476
Marcio <marcioabcd11@gmail.com>
2021-11-30 13:51:27 -0300

    deploy fix

--- src/services/service.baseURL.js
+++ src/services/service.baseURL.js
@@ -1,7 +1,6 @@
-import process from 'process';
-
+/* eslint-disable no-undef */
 const BASE_URL = (process.env.REACT_APP_NODE_ENV === 'production')
-  ? ' https://zapper-marcio.herokuapp.com'
+  ? 'https://zapper-marcio.herokuapp.com'
   : 'http://localhost:4000';
 
 export default BASE_URL;
